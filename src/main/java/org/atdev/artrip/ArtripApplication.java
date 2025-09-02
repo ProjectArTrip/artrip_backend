bac49ac25557bc04c34f3907567982e8526f2558
wook099 <codnrud99@naver.com>
2025-09-02 20:06:24 +0900

    initial

---
+++ src/main/java/org/atdev/artrip/ArtripApplication.java
@@ -0,0 +1,13 @@
+package org.atdev.artrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtripApplication.class, args);
+    }
+
+}
