9e385ac3d5e8aee3b48b90b672d0710419af53b0
wallace <j76067469@gmail.com>
2025-09-10 09:09:20 +0000

    docker init

---
+++ src/main/java/org/atdev/artrip/greeting/StatusController.java
@@ -0,0 +1,13 @@
+package org.atdev.artrip.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/")
+    public StatusResponse getStatus() {
+        return new StatusResponse("200", "greeting");
+    }
+}
