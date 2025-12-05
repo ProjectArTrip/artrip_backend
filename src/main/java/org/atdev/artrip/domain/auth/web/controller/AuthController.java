844fa91a48ec19d58ca906a430b03915b887f734
이선호 <wallace@sellmate.co.kr>
2025-12-05 22:59:22 +0900

    refactor: Enhance error response handling in GlobalOperationCustomizer

--- src/main/java/org/atdev/artrip/domain/auth/web/controller/AuthController.java
+++ src/main/java/org/atdev/artrip/domain/auth/web/controller/AuthController.java
@@ -45,7 +45,12 @@ public class AuthController {
 
     @Operation(summary = "토큰 재발행 (앱 전용)", description = "refresh토큰으로 access토큰을 재발행합니다")
     @ApiErrorResponses(
-            user = {UserError._USER_NOT_FOUND, UserError._INVALID_REFRESH_TOKEN, UserError._INVALID_USER_REFRESH_TOKEN},
+            user = {
+                    UserError._USER_NOT_FOUND,
+                    UserError._INVALID_REFRESH_TOKEN,
+                    UserError._INVALID_USER_REFRESH_TOKEN,
+                    UserError._JWT_EXPIRED_REFRESH_TOKEN,
+            },
             common = {CommonError._BAD_REQUEST, CommonError._UNAUTHORIZED, CommonError._INTERNAL_SERVER_ERROR}
     )
     @PostMapping("/app/reissue")
@@ -85,7 +90,14 @@ public class AuthController {
 
     @Operation(summary = "소셜 SDK 토큰 검증 후 jwt 발급", description = "만료일 : refresh: 7일 , access: 15분 ,isFirstLogin true:회원가입 false:로그인")
     @ApiErrorResponses(
-            user = {UserError._SOCIAL_ID_TOKEN_INVALID, UserError._USER_NOT_FOUND, UserError._SOCIAL_VERIFICATION_FAILED, UserError._SOCIAL_TOKEN_EXPIRED,UserError._SOCIAL_TOKEN_INVALID_SIGNATURE,UserError._SOCIAL_TOKEN_INVALID_AUDIENCE},
+            user = {
+                    UserError._SOCIAL_ID_TOKEN_INVALID,
+                    UserError._USER_NOT_FOUND,
+                    UserError._SOCIAL_VERIFICATION_FAILED,
+                    UserError._SOCIAL_TOKEN_EXPIRED,
+                    UserError._SOCIAL_TOKEN_INVALID_SIGNATURE,
+                    UserError._SOCIAL_TOKEN_INVALID_AUDIENCE,
+            },
             common = {CommonError._BAD_REQUEST, CommonError._UNAUTHORIZED, CommonError._INTERNAL_SERVER_ERROR}
     )
     @PostMapping("/social")
