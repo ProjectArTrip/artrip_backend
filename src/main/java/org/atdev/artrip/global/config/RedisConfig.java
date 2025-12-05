2a2997353f4e7defb71eb4b4671963afc4b7438f
이선호 <wallace@sellmate.co.kr>
2025-12-06 02:56:06 +0900

    Refactoring: Changing Redis Configurations

--- src/main/java/org/atdev/artrip/global/config/RedisConfig.java
+++ src/main/java/org/atdev/artrip/global/config/RedisConfig.java
@@ -1,11 +1,17 @@
 package org.atdev.artrip.global.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -13,7 +19,22 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
+    @Primary
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setDatabase(0);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean(name = "redisTemplate")
+    @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
@@ -22,5 +43,22 @@ public class RedisConfig {
         return template;
     }
 
+    @Bean(name = "jwtRedisTemplate")
+    public StringRedisTemplate jwtRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean(name = "recommendRedisConnectionFactory")
+    public RedisConnectionFactory recommendRedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setDatabase(1);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean(name = "recommendRedisTemplate")
+    public StringRedisTemplate recommendStringRedisTemplate(@Qualifier("recommendRedisConnectionFactory")
+                                                            RedisConnectionFactory recommendRedisConnectionFactory) {
+        return new StringRedisTemplate(recommendRedisConnectionFactory);
+    }
 
 }
