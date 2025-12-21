3f137efe3aa153ed20a89d4edd7dc55b6e347b4e
wook099 <codnrud99@naver.com>
2025-12-21 17:24:10 +0900

    [CHORE] 나의 취향분석 api 파일 위치 변경 유저키워드->유저

--- src/main/java/org/atdev/artrip/domain/user/service/UserService.java
+++ src/main/java/org/atdev/artrip/domain/user/service/UserService.java
@@ -2,9 +2,49 @@ package org.atdev.artrip.domain.user.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.atdev.artrip.domain.auth.data.User;
+import org.atdev.artrip.domain.auth.repository.UserRepository;
+import org.atdev.artrip.domain.keyword.data.Keyword;
+import org.atdev.artrip.domain.keyword.data.UserKeyword;
+import org.atdev.artrip.domain.keyword.repository.KeywordRepository;
+import org.atdev.artrip.domain.keyword.repository.UserKeywordRepository;
+import org.atdev.artrip.global.apipayload.code.status.UserError;
+import org.atdev.artrip.global.apipayload.exception.GeneralException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final KeywordRepository keywordRepository;
+    private final UserKeywordRepository userKeywordRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void saveUserKeywords(Long userId, List<Long> keywordIds) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new GeneralException(UserError._USER_NOT_FOUND));
+
+        userKeywordRepository.deleteByUser(user);
+
+        List<Keyword> keywords = keywordRepository.findAllById(keywordIds);
+
+        List<UserKeyword> userKeywords = keywords.stream()
+                .map(keyword -> UserKeyword.builder()
+                        .user(user)
+                        .keyword(keyword)
+                        .createdAt(LocalDateTime.now())
+                        .build())
+                .toList();
+
+        userKeywordRepository.saveAll(userKeywords);
+    }
+
+
 }
