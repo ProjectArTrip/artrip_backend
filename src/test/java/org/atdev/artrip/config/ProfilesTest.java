c601cbb04f610d88941083c574875c7436475768
이선호 <j76067469@gmail.com>
2026-01-06 03:29:38 +0900

    test: add database connection test to ProfilesTest

--- src/test/java/org/atdev/artrip/config/ProfilesTest.java
+++ src/test/java/org/atdev/artrip/config/ProfilesTest.java
@@ -1,17 +1,20 @@
 package org.atdev.artrip.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @SpringBootTest
 @ActiveProfiles({"common", "local"})
@@ -23,6 +26,9 @@ public class ProfilesTest {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     @DisplayName("프로파일 설정값 확인")
     void localConfigurationLoadTest() {
@@ -33,6 +39,21 @@ public class ProfilesTest {
 
         //then
         assertThat(activeProfiles).contains("common", "local");
+    }
+
+    @Test
+    @DisplayName("데이터 베이스 연결 확인")
+    void databaseConnectionTest() {
+        //given
+
+        //when
+        try (Connection connection = dataSource.getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.isClosed()).isFalse();
+            System.out.println("연결 성공: " + connection.getMetaData().getURL());
+        } catch (SQLException e) {
+            fail("실패 : " + e.getMessage());
+        }
     }
 
 }
