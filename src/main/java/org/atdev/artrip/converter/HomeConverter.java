91de20fef1ee7c75aaa470b54bce831abcfba431
wook099 <codnrud99@naver.com>
2026-01-19 23:52:47 +0900

    [CHORE] 사용하지 않는 컨버터 삭제

--- src/main/java/org/atdev/artrip/converter/HomeConverter.java
+++ src/main/java/org/atdev/artrip/converter/HomeConverter.java
@@ -14,39 +14,6 @@ import java.util.Set;
 @Component
 public class HomeConverter {
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-
-    public FilterResponse toFilterResponse(Slice<Exhibit> slice, Set<Long> favorites) {
-
-        List<HomeListResponse> postDtos = slice.getContent()
-                .stream()
-                .map(exhibit -> toHomeExhibitListResponse(exhibit, favorites.contains(exhibit.getExhibitId())))
-                .toList();
-
-        Long nextCursor = slice.hasNext() && !slice.isEmpty()
-                ? slice.getContent().get(slice.getContent().size() - 1).getExhibitId()
-                : null;
-
-        return new FilterResponse(postDtos, slice.hasNext(), nextCursor);
-    }
-
-
-    public HomeListResponse toHomeExhibitListResponse(Exhibit exhibit, boolean isFavorite) {
-
-        String period = exhibit.getStartDate().format(formatter) + " - " + exhibit.getEndDate().format(formatter);
-
-        return HomeListResponse.builder()
-                .exhibitId(exhibit.getExhibitId())
-                .title(exhibit.getTitle())
-                .posterUrl(exhibit.getPosterUrl())
-                .status(exhibit.getStatus())
-                .exhibitPeriod(period)
-                .isFavorite(isFavorite)
-                .build();
-    }
-
-
-
 
     public ExhibitRecentResponse toExhibitRecentResponse(Exhibit exhibit){
 
