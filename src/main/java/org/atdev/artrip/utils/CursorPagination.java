b1de3a5f909c8b0ed920017140ade84e1979c07a
이선호 <j76067469@gmail.com>
2026-03-19 00:37:25 +0900

    fix: Conflict해결

--- src/main/java/org/atdev/artrip/utils/CursorPagination.java
+++ src/main/java/org/atdev/artrip/utils/CursorPagination.java
@@ -12,18 +12,10 @@ public record CursorPagination(
         @Min(value = 1, message = "최소값 1")
         @Max(value = 20, message = "최대값 20")
         Long size
-
-<<<<<<< ART-273
 ) {
     public CursorPagination {
         if (size == null) {
             size = 20L;
         }
     }
-=======
-    @Min(value = 1, message = "최소값 1")
-    @Max(value = 20, message = "최대값 20")
-    private Long size = 20L;
-
->>>>>>> developer
 }
