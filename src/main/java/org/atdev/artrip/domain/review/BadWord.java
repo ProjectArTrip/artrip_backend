5c968c1283ba982e8a914d1be8a78df2e89176ec
wook099 <codnrud99@naver.com>
2026-04-15 14:43:52 +0900

    [FEAT] BadWord 엔티티 생성

---
+++ src/main/java/org/atdev/artrip/domain/review/BadWord.java
@@ -0,0 +1,32 @@
+package org.atdev.artrip.domain.review;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class BadWord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String word;
+
+    @Column(nullable = false)
+    private boolean useRegex;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
