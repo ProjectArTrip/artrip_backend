1b7fe1622ae4adf15cfb433f938a9b5269a7edee
wook099 <codnrud99@naver.com>
2026-04-22 21:12:31 +0900

    [FIX] clearSession TransactionalEventListener내부로 이동

--- src/main/java/org/atdev/artrip/service/event/WithdrawEventListener.java
+++ src/main/java/org/atdev/artrip/service/event/WithdrawEventListener.java
@@ -2,7 +2,8 @@ package org.atdev.artrip.service.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.atdev.artrip.service.AuthService;
+import org.atdev.artrip.jwt.JwtProvider;
+import org.atdev.artrip.service.redis.RedisService;
 import org.atdev.artrip.validator.social.SocialVerifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -15,11 +16,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WithdrawEventListener {
 
-    private final AuthService authService;
     private final List<SocialVerifier> socialVerifiers;
+    private final RedisService redisService;
+    private final JwtProvider jwtProvider;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(WithdrawEvent event) {
+
+        try {
+            clearSession(event.accessToken(), event.refreshToken());
+        } catch (Exception e) {
+            log.error("세션 초기화 실패 userId={}", event.userId(), e);
+        }
+
         for (WithdrawEvent.SocialInfo social : event.socials()) {
             socialVerifiers.stream()
                     .filter(v -> v.getProvider() == social.provider())
@@ -33,5 +42,13 @@ public class WithdrawEventListener {
                         }
                     });
         }
+    }
+
+    private void clearSession(String accessToken, String refreshToken) {
+        long remainTime = jwtProvider.getExpiration(accessToken);
+        if (remainTime > 0) {
+            redisService.save("BLACKLIST:" + accessToken, "logout", remainTime);
+        }
+        redisService.deleteKey(refreshToken);
     }
 }
