3d8cb7c3a6a1a9d1552fdd4c61799dda6db2ff38
wook099 <codnrud99@naver.com>
2026-04-23 22:24:34 +0900

    [REFACTOR] notice 테이블 user nullable=true

--- src/main/java/org/atdev/artrip/domain/notice/Notice.java
+++ src/main/java/org/atdev/artrip/domain/notice/Notice.java
@@ -26,7 +26,7 @@ public class Notice {
     private Long noticeId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
     @Enumerated(EnumType.STRING)
