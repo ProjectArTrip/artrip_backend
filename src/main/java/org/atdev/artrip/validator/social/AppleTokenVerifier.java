fbc3c404a87bbec5efd0cda07f60164df498b0f4
wook099 <codnrud99@naver.com>
2026-05-11 22:50:47 +0900

    [FIX] 암호 알고리즘 변경

--- src/main/java/org/atdev/artrip/validator/social/AppleTokenVerifier.java
+++ src/main/java/org/atdev/artrip/validator/social/AppleTokenVerifier.java
@@ -24,7 +24,7 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URL;
 import java.security.KeyFactory;
 import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
@@ -68,7 +68,7 @@ public class AppleTokenVerifier implements SocialVerifier {
             String kid = decodedJWT.getKeyId();
 
             Jwk jwk = jwkProvider.get(kid);
-            Algorithm algorithm = Algorithm.ECDSA256((ECPublicKey) jwk.getPublicKey(), null);
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
 
             JWTVerifier verifier = JWT.require(algorithm)
                     .withIssuer(APPLE_ISSUER)
